b751a0c02dcec82ee3e28107821237833392d1aa
libre <18818927425@163.com>
2024-11-25 15:11:30 +0800

    update: change schedule time

--- celery_config.py
+++ celery_config.py
@@ -40,7 +40,7 @@ beat_schedule = {
     },
     'video': {
         'task': 'video',
-        'schedule': crontab(minute='0', hour='*/6')
+        'schedule': crontab(minute='00', hour='0')
     },
     'logistics_9:00': {
         'task': 'logistics',
